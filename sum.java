6ac937a58200a442860b014d128aee5e697f1b5d
durga-12-hub <db543952@gmail.com>
2026-08-28 22:48:51 +0530

    Add sum of natural numbers program

---
+++ sum.java
@@ -0,0 +1,19 @@
+public class SumOfNumbers {
+
+    public static void sumOfNaturalNumbers(int n) {
+        int sum = 0;
+        int counter = 1;
+
+        while (counter <= n) {
+            sum += counter;
+            counter++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+        // Test case from sample output
+        sumOfNaturalNumbers(5);
+    }
+}
