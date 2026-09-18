9b65d92fb641dafaf97f779fca9d81d5069c599a
durga-12-hub <db543952@gmail.com>
2026-09-19 02:05:50 +0530

    Add ass54.java

---
+++ ass54.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class ass54 {
+
+    static double rowAverage(int[] row) {
+
+        int sum = 0;
+
+        for (int value : row) {
+            sum += value;
+        }
+
+        return (double) sum / row.length;
+    }
+
+    static String classifyMatches(int[][] runsPerOver, int threshold) {
+
+        String result = "";
+
+        for (int i = 0; i < runsPerOver.length; i++) {
+
+            double average = rowAverage(runsPerOver[i]);
+
+            if (average >= threshold) {
+                result += "Match " + i + ": Power Surge";
+            } else {
+                result += "Match " + i + ": Normal";
+            }
+
+            if (i < runsPerOver.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of matches: ");
+        int matches = sc.nextInt();
+
+        int[][] runs = new int[matches][];
+
+        for (int i = 0; i < matches; i++) {
+
+            System.out.print("Enter number of overs for Match " + i + ": ");
+            int overs = sc.nextInt();
+
+            runs[i] = new int[overs];
+
+            System.out.println("Enter runs:");
+
+            for (int j = 0; j < overs; j++) {
+                runs[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter threshold: ");
+        int threshold = sc.nextInt();
+
+        System.out.println(classifyMatches(runs, threshold));
+
+        sc.close();
+    }
+}
